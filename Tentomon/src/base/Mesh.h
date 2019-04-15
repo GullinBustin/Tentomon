30ed5ab996209c9330d491ab99da58d8c8188ca1
Javier Portillo Medina <javierportillo93@gmail.com>
2019-04-16 00:07:30 +0200

    Add base/Mesh class

--- Tentomon/src/base/Mesh.h
+++ Tentomon/src/base/Mesh.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <vector>
+
+#include "Shader.h"
+
 class Mesh
 {
 public:
-	Mesh();
+	Mesh(std::vector<float> vertices, std::vector<unsigned int> indices);
 	~Mesh();
+	void draw(Shader shader);
+	unsigned int VAO;
+
+private:
+
+	std::vector<float> vertices;
+	std::vector<unsigned int> indices;
+	unsigned int VBO, EBO;
 };
 
